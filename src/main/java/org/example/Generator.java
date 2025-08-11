994c65405b12777c4dcb15255bf2051e442ad2ec
Gabriel Perin <works.gp99@gmail.com>
2025-08-11 20:45:08 -0300

    Code Logic Improvement | Added option to add custom words | Added checkbox logic for interface

--- src/main/java/org/example/Generator.java
+++ src/main/java/org/example/Generator.java
@@ -3,9 +3,11 @@ package org.example;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.security.SecureRandom;
@@ -13,9 +15,22 @@ import java.util.Base64;
 
 import javafx.scene.Scene;
 
+
 public class Generator extends Application {
 
-    private static String genString(int length){
+    private static String partialPasswordGenerator(String word) {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[3];
+        random.nextBytes(bytes);
+        byte[] bytes1 = new byte[3];
+        random.nextBytes(bytes1);
+        String encoded = Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+        String encoded2 = Base64.getUrlEncoder().withoutPadding().encodeToString(bytes1);
+
+        return encoded+word+encoded2;
+    }
+
+    private static String totalPasswordGenerator(int length){
         SecureRandom randomString = new SecureRandom();
         byte[] bytes = new byte[length];
         randomString.nextBytes(bytes);
@@ -28,36 +43,78 @@ public class Generator extends Application {
     public void start(Stage stage){
 
         Label title = new Label("Gerador de Senhas Customizavel");
-        Text lengthDescription = new Text("Quantidade de Caracteres: ");
-        TextField length = new TextField();
-        Button btn = new Button("Gerar Senha!");
-        Label result = new Label();
 
+        //objects belonging to the total password generation method:
+        CheckBox totalPass = new CheckBox("Gerador Total");
+        Text totalDescription = new Text("Quantidade de Caracteres: ");
+        TextField totalLengthValue = new TextField();
+        Button btnTotal = new Button("Gerar Senha!");
+
+        //Objects belonging to the partial password generation method:
+        CheckBox partialPass = new CheckBox("Adicione uma Palavra");
+        Text wordInfo = new Text("Digite sua Palavra:");
+        TextField word = new TextField();
+        Button btn2 = new Button("Gerar Senha!");
+
+
+        TextField result = new TextField("Resultado será Impresso aqui!");
+
+        HBox hBox = new HBox(totalPass,partialPass);
+        hBox.setSpacing(15);
         GridPane layout = new GridPane();
         layout.setAlignment(Pos.CENTER);
         layout.setHgap(15);
         layout.setVgap(5);
-
         layout.add(title, 0,0,3,1);
-        layout.addRow(1,lengthDescription);
-        layout.add(length,1,1,2,1);
-        layout.add(btn,1,3);
-        layout.add(result, 0,4,3,1);
+        layout.add(hBox,1,1);
 
-        btn.setOnAction(_ -> {
+        totalPass.setOnAction(_ ->{
+            layout.getChildren().removeAll(totalLengthValue,totalDescription, btnTotal,word,wordInfo,btn2);
+
+            if (totalPass.isSelected()) {
+                partialPass.setSelected(false);
+                layout.addRow(2, totalDescription);
+                layout.add(totalLengthValue, 1, 2, 2, 1);
+                layout.add(btnTotal, 1, 3);}
+            });
+
+        partialPass.setOnAction(_ -> {
+
+            layout.getChildren().removeAll(totalLengthValue,totalDescription, btnTotal, word, wordInfo, btn2);
+
+            if (partialPass.isSelected()) {
+                totalPass.setSelected(false);
+                layout.addRow(2, wordInfo);
+                layout.add(word, 1, 2, 2, 1);
+                layout.add(btn2, 1, 3);
+            }
+        });
+
+        layout.add(result, 0,4,5,1);
+
+        btnTotal.setOnAction(_ -> {
             try{
-                int getLenght = Integer.parseInt(length.getText());
+                int getLenght = Integer.parseInt(totalLengthValue.getText());
                 if (getLenght <= 0){
                     result.setText("Digite um número maior que zero!");
                     return;
                 }
-                result.setText(genString(getLenght));
+                result.setText(totalPasswordGenerator(getLenght));
             } catch (NumberFormatException e) {
                 result.setText("Digite um número válido!");
             }
         });
 
-        Scene scene = new Scene(layout, 300,240);
+        btn2.setOnAction(_->{
+            try{
+                String getWord = word.getText();
+                result.setText(partialPasswordGenerator(getWord));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Scene scene = new Scene(layout, 500,400);
         stage.setTitle("Gerador de Senhas");
         stage.setScene(scene);
         stage.show();
